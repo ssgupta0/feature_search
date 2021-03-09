28a8089d9add1cbb5c8c583150ebc32a4d9f9ab9
ssgupta2 <shashvatguptausa@gmail.com>
2021-03-09 13:18:43 -0800

    basic file input and var setup

--- feature_search/main.cpp
+++ feature_search/main.cpp
@@ -10,11 +10,50 @@
 #include <fstream>
 
 
+
+
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    std::string input;
+    
+    float lung;
+    
+    float height;
+    float years;
+    float weight;
+    float cigs;
+    
+    // open input file
+    std::ifstream myfile("movieReviews.txt");
+    if (myfile.fail()) {
+        std::cout << "could not open file" << std::endl;
+        exit(1);
+    }
     
     
+    //parse through file input
+    while (!myfile.eof()) {
+        myfile >> input;                //input value
+        lung = std::stof(input);      //convert to float
+        myfile.get();                   //get blank space
+        
+        myfile >> input;                //input value
+        height = std::stof(input);      //convert to float
+        myfile.get();                   //get blank space
+        
+        myfile >> input;                //input value
+        years = std::stof(input);      //convert to float
+        myfile.get();                   //get blank space
+        
+        myfile >> input;                //input value
+        weight = std::stof(input);      //convert to float
+        myfile.get();                   //get blank space
+        
+        myfile >> input;                //input value
+        cigs = std::stof(input);      //convert to float
+        myfile.get();                   //get blank space
+    }
     
     return 0;
 }
