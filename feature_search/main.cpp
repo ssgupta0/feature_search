76c851578911e28661d1f1f1bb2e0b0c4f2e5732
ssgupta2 <shashvatguptausa@gmail.com>
2021-03-09 15:38:48 -0800

    trying to pass file

--- feature_search/main.cpp
+++ feature_search/main.cpp
@@ -7,32 +7,37 @@
 //
 
 #include <iostream>
+//#include <bits/stdc++.h>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <sstream>
 
 
-void feature_search() {
-    //for(int  i = 0; i < )
+void feature_search(std::vector<std::vector<float>> data) {
+    for(int  i = 0; i < data.size()-1; i++) {
+        if(i==1) {
+            std::cout << "On the 1st level of the search tree" << std::endl;
+        }
+        else {
+        std::cout << "On the " << i << "th level of the search tree" << std::endl;
+        }
+    }
 }
 
 int main(int argc, const char * argv[]) {
 
     std::string input;
     
+    std::string fileName = "CS170_SMALLtestdata__75.txt";
+    //std::string fileName = "CS170_largetestdata__26.txt";
+
+    std::string line;
+    std::vector<std::vector<float>> data;
     
-    //lungs
-    
-    //height
-    //years
-    //weight
-    //cigs
-    
-    std::vector<std::vector<float>> data {
-        {},{},{},{},{}
-    };
     
     // open input file
-    std::ifstream myfile("movieReviews.txt");
+    std::ifstream myfile(fileName);
     if (myfile.fail()) {
         std::cout << "could not open file" << std::endl;
         exit(1);
@@ -40,26 +45,12 @@ int main(int argc, const char * argv[]) {
     
     
     //parse through file input
-    while (!myfile.eof()) {
-        myfile >> input;                             //input value
-        data.at(0).push_back(std::stof(input));      //convert to float
-        myfile.get();                                //get blank space
+    while (std::getline(myfile, line)) {
+        std::istringstream iss(line);
         
-        myfile >> input;                             //input value
-        data.at(1).push_back(std::stof(input));      //convert to float
-        myfile.get();                                //get blank space
-        
-        myfile >> input;                             //input value
-        data.at(2).push_back(std::stof(input));      //convert to float
-        myfile.get();                                //get blank space
-        
-        myfile >> input;                             //input value
-        data.at(3).push_back(std::stof(input));      //convert to float
-        myfile.get();                                //get blank space
-        
-        myfile >> input;                             //input value
-        data.at(4).push_back(std::stof(input));      //convert to float
-        myfile.get();                                //get blank space
+        for(int i = 0; std::getline(iss, input, ' '); i++) {
+            data.at(i).push_back(std::stof(input));      //convert to float
+        }
     }
     
     return 0;
